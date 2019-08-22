9b7fe8979d9059f12a6f607eeef3bee97a8d3841
MarinesLemus007 <marineslemus@gmail.com>
2019-08-22 18:12:01 -0400

    Updates

---
+++ precache-manifest.5e9ef60158b616ca5d81a67374f75250.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95e513ec465e90a802328e597cb4c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bef6650a21fa340913cd",
+    "url": "/static/css/main.3302f29d.chunk.css"
+  },
+  {
+    "revision": "aa57165b57098f0c0748",
+    "url": "/static/js/2.c7979310.chunk.js"
+  },
+  {
+    "revision": "bef6650a21fa340913cd",
+    "url": "/static/js/main.16140425.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  },
+  {
+    "revision": "da5bd174c27cfa3552c19bcba59e5820",
+    "url": "/static/media/burger.da5bd174.png"
+  },
+  {
+    "revision": "adf7e77b1e6dcdaaf393354ebeec7bc5",
+    "url": "/static/media/developer.adf7e77b.jpg"
+  },
+  {
+    "revision": "4c0602210d21c9be3b0fafe053879345",
+    "url": "/static/media/logo.4c060221.png"
+  },
+  {
+    "revision": "ad00550378d92ba4da8eb7f16bb392fa",
+    "url": "/static/media/yo.ad005503.jpg"
+  }
+]);
